9a8a63201106cedad69ec3aa08d70772172d1e57
Filip Markovic <filip98122@gmail.com>
2024-07-15 20:02:13 +0200

    Highscore set! <3

--- main.py
+++ main.py
@@ -19,14 +19,13 @@ def colision1(rect1 : pygame.Rect,rect2):
         return True
     return False
 clock = pygame.time.Clock()
-WIDTH,HEIGHT = 800,800
+WIDTH,HEIGHT = 765,765
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 def button_colision(width,height,x,y,mousePos,mouseState):
     if mousePos[0] > x and mousePos[0] < x + width and mousePos[1] > y and mousePos[1] < y + height and mouseState[0] == True:
         return True
     else:
         return False
-
 class Part:
     def __init__(self,x,y,rad,id):
         self.x = x
@@ -44,8 +43,6 @@ class Part:
         if self.id == "body":
                 pygame.draw.circle(window,pygame.Color(255,255,0),(self.x,self.y),self.rad)
                 pygame.draw.circle(window,pygame.Color(255,100,10),(self.x,self.y),self.rad-10)
-
-
 class Snake:
     def __init__(self,rad,krugovi,x,y):
         self.rad = rad
@@ -62,7 +59,6 @@ class Snake:
     def draw(self,window):
         for i in range(len(self.body)-1,-1,-1):
             self.body[i].draw(window)
-            
     def move(self, keys):
         if len(self.head_pos)>self.amount_to_track:
             del self.head_pos[0]
@@ -83,7 +79,9 @@ class Snake:
         if keys[pygame.K_LEFT] and self.body[0].dx != self.speed or keys[pygame.K_a] and self.body[0].dx != self.speed:
             self.body[0].dx = -self.speed
             self.body[0].dy = 0
-        
+        if self.body[0].x < self.rad or self.body[0].x > WIDTH-self.rad or self.body[0].y < self.rad or self.body[0].y > HEIGHT-self.rad:
+            shutdown()
+            
         self.head_pos.append([self.body[0].x,self.body[0].y])
 class Apple:
     def __init__(self,x,y,rad):
@@ -98,20 +96,45 @@ class Apple:
         self.width = self.scaled_img.get_width()
         self.height =self.scaled_img.get_height()
         window.blit(self.scaled_img, (self.x - self.width/2, self.y - self.height/2))
-        
-
-
-class text:
+class Text:
     def __init__(self,window):
         self.myfont = pygame.font.SysFont('Comic Sans MS', 70)
         self.myfont1 = pygame.font.SysFont('Comic Sans MS', 15)
         self.window = window
-    def draw(self):
-        pass
+    def draw(self,kru,score):
+        text_surface = self.myfont1.render(f"Score: {int(kru-5)}", True, (0, 0, 0))
+        self.window.blit(text_surface,(10,10))
+        hs = read()
+        if hs<score:
+            hs = score
+        text_surface1 = self.myfont1.render(f"Highscore: {int(hs)}", True, (0, 0, 0))
+        self.window.blit(text_surface1,(10,30))
 s1 = Snake(30,5,100,100)
 apple = []
 score = 0
 game = 1
+
+def read():
+    f = open("test.txt", "r")
+    highscore = f.read()
+    hk = int(highscore)
+    f.close()
+    return hk
+    
+def write(highscore):
+    f = open("test.txt", "w")
+    f.write(str(highscore))
+    hk = int(highscore)
+    f.close()
+    return hk
+
+def shutdown():
+    hs = read()
+    if score > hs:
+        write(score)
+    exit()
+
+t1 = Text(window)
 x_a = random.randint(s1.rad+s1.rad//2,WIDTH-s1.rad-s1.rad//2)
 y_a = random.randint(s1.rad+s1.rad//2,HEIGHT-s1.rad-s1.rad//2)
 a1 = Apple(x_a,y_a,35)
@@ -122,23 +145,24 @@ while game:
     events = pygame.event.get()
     mouseState = pygame.mouse.get_pressed()
     mousePos = pygame.mouse.get_pos()
-
     for event in events:
-        if event.type == pygame.QUIT:            exit()
-    if keys[pygame.K_ESCAPE]:        exit()
+        if event.type == pygame.QUIT:
+            shutdown()
+            
+    if keys[pygame.K_ESCAPE]:
+        shutdown()
+        
     if collison(a1.x,a1.y,a1.rad,s1.body[0].x,s1.body[0].y,s1.rad):
         x_a = random.randint(s1.rad+s1.rad//2,WIDTH-s1.rad-s1.rad//2)
         y_a = random.randint(s1.rad+s1.rad//2,HEIGHT-s1.rad-s1.rad//2)
         a1 = Apple(x_a,y_a,35)
         s1.krugovi += 1
-        #s1.body.append(Part(s1.head_pos[len(s1.head_pos)-60*(s1.krugovi)]-s1.distance*i,y,s1.rad,"body"))
         s1.body.append(Part(0,0,s1.rad,"body"))
         s1.amount_to_track+=60
         score+=1
     s1.move(keys)
-    
     s1.draw(window)
     a1.draw(window)
+    t1.draw(s1.krugovi,score)
     pygame.display.update()
     clock.tick(60)
-    
